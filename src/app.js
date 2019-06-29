cbfcfeedd86278e76a36f210e503b6ff0966b24b
ak1103dev <ak.apichan1103@gmail.com>
2019-06-29 12:05:35 +0700

    :construction: can get token

--- src/app.js
+++ src/app.js
@@ -1,7 +1,28 @@
 import express from 'express'
+import twilio from 'twilio'
 
 const app = express()
 
-app.use('/', (req, res) => res.send('Hello, My API'))
+const AccessToken = twilio.jwt.AccessToken
+const VideoGrant = AccessToken.VideoGrant
+
+app.get('/', (req, res) => res.send('Hello, My API'))
+
+app.get('/token', (req, res) => {
+  const identity = Math.floor(Math.random() * 1001).toString()
+  const token = new AccessToken(
+    process.env.TWILIO_ACCOUNT_SID,
+    process.env.TWILIO_API_KEY,
+    process.env.TWILIO_API_SECRET,
+  )
+  token.identity = identity
+  const grant = new VideoGrant()
+  token.addGrant(grant)
+  const data = {
+    identity: identity,
+    token: token.toJwt()
+  }
+  res.send(data)
+})
 
 export default app
